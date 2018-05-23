072906ad098cecebf5a5c9a593c1e31264da63e6
Aleksander Gajda <aleksander.gajda5@gmail.com>
2018-05-23 20:28:12 +0200

    Complete

---
+++ js/custom.js
@@ -0,0 +1,147 @@
+
+jQuery(function($){
+
+
+	/* ----------------------------------------------------------- */
+	/*  1. Superslides Slider
+	/* ----------------------------------------------------------- */
+	jQuery('#slides').superslides({
+      animation: 'slide',
+      play: '5000'
+    });
+	
+	
+	/* ----------------------------------------------------------- */
+	/*  2. Fixed Top Menubar
+	/* ----------------------------------------------------------- */
+
+	// For fixed top bar
+       $(window).scroll(function(){
+        if($(window).scrollTop() >100 /*or $(window).height()*/){
+            $(".navbar-fixed-top").addClass('past-main');   
+        }
+    else{    	
+      $(".navbar-fixed-top").removeClass('past-main');
+      }
+    });
+
+
+	/* ----------------------------------------------------------- */
+	/*  3. Wow smooth animation
+	/* ----------------------------------------------------------- */
+
+	wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100
+      }
+    );
+    wow.init();
+
+
+	/* ----------------------------------------------------------- */
+	/* 4. OPINIE SLIDER
+	/* ----------------------------------------------------------- */
+
+	$('.opinie_slider').slick({
+      dots: true,
+      infinite: true,      
+      speed: 800,
+      arrows:false,      
+      slidesToShow: 1,
+      slide: 'li',
+      autoplay: true,
+      fade: true,
+      autoplaySpeed: 3000,
+      cssEase: 'linear'
+    });
+
+
+ 
+
+	/* ----------------------------------------------------------- */
+	/*  5. SCROLL TOP BUTTON
+	/* ----------------------------------------------------------- */
+
+	//Check to see if the window is top if not then display button
+
+	  $(window).scroll(function(){
+	    if ($(this).scrollTop() > 300) {
+	      $('.scrollToTop').fadeIn();
+	    } else {
+	      $('.scrollToTop').fadeOut();
+	    }
+	  });
+	   
+	  //Click event to scroll to top
+
+	  $('.scrollToTop').click(function(){
+	    $('html, body').animate({scrollTop : 0},800);
+	    return false;
+	  });
+
+
+	/* ----------------------------------------------------------- */
+	/*  6. MENU SCROLL 
+	/* ----------------------------------------------------------- */
+
+	//MENU SCROLLING WITH ACTIVE ITEM SELECTED
+
+	// Cache selectors
+	var lastId,
+	topMenu = $("#top-menu"),
+	topMenuHeight = topMenu.outerHeight()+13,
+	// All list items
+	menuItems = topMenu.find("a"),
+	// Anchors corresponding to menu items
+	scrollItems = menuItems.map(function(){
+	  var item = $($(this).attr("href"));
+	  if (item.length) { return item; }
+	});
+
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+	menuItems.click(function(e){
+	  var href = $(this).attr("href"),
+	      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+	  $('html, body').stop().animate({ 
+	      scrollTop: offsetTop
+	  }, 900);
+	  e.preventDefault();
+	});
+
+	// Bind to scroll
+	$(window).scroll(function(){
+	   // Get container scroll position
+	   var fromTop = $(this).scrollTop()+topMenuHeight;
+	   
+	   // Get id of current scroll item
+	   var cur = scrollItems.map(function(){
+	     if ($(this).offset().top < fromTop)
+	       return this;
+	   });
+	   // Get the id of the current element
+	   cur = cur[cur.length-1];
+	   var id = cur && cur.length ? cur[0].id : "";
+	   
+	   if (lastId !== id) {
+	       lastId = id;
+	       // Set/remove active class
+	       menuItems
+	         .parent().removeClass("active")
+	         .end().filter("[href=#"+id+"]").parent().addClass("active");
+	   }           
+	})
+
+
+	/* ----------------------------------------------------------- */
+	/*  7. MOBILE MENU ACTIVE CLOSE 
+	/* ----------------------------------------------------------- */ 
+
+	$('.navbar-nav').on('click', 'li a', function() {
+	  $('.navbar-collapse').collapse('hide');
+	});
+
+	
+});
+
